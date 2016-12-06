3678f083603cd19c364bec1eb4b6a31b00e34322
David Bock <dbock@codesherpas.com>
2016-12-06 09:49:21 -0500

    this might be a compatible alternative.  Need to figure out how to test without going through a commit/tap cycle.

--- zork1.rb
+++ zork1.rb
@@ -6,9 +6,9 @@ class Zork1 < Formula
   homepage 'http://www.infocom-if.org/downloads/downloads.html'
   url 'http://www.infocom-if.org/downloads/zork1.zip'
   version '1'
-  #sha1 'c0792003a0f4f9ae5f7d058c95142ad2cb052e16'
+  sha256 '645027fb189ba8fd30daecca609e68e2bf2337b76e7b4f925ccf58778f3eac40'
 
-  #depends_on 'jzip'
+  depends_on 'p7zip'
 
   def install
     def copy_without(source_path, target_path, exclude)
@@ -27,7 +27,7 @@ class Zork1 < Formula
     copy_without('.', './share/zork1', 'share')
     file = File.new("zork1", "w+", 0755)
     file.write "#!/bin/bash
-zip #{prefix}/share/zork1/DATA/ZORK1.DAT
+7za #{prefix}/share/zork1/DATA/ZORK1.DAT
 "
     bin.install "zork1"
     share.install "share/zork1"
