ac702c750bac15b7cc58c13a9cd4cda6c48581df
David Bock <dbock@codesherpas.com>
2016-12-06 10:12:29 -0500

    forced jzip locally... lets see if zork works before plodding ahead

--- zork1.rb
+++ zork1.rb
@@ -8,7 +8,7 @@ class Zork1 < Formula
   version '1'
   sha256 '645027fb189ba8fd30daecca609e68e2bf2337b76e7b4f925ccf58778f3eac40'
 
-  depends_on 'p7zip'
+  #depends_on 'jzip'
 
   def install
     def copy_without(source_path, target_path, exclude)
@@ -27,7 +27,7 @@ class Zork1 < Formula
     copy_without('.', './share/zork1', 'share')
     file = File.new("zork1", "w+", 0755)
     file.write "#!/bin/bash
-7za #{prefix}/share/zork1/DATA/ZORK1.DAT
+jzip #{prefix}/share/zork1/DATA/ZORK1.DAT
 "
     bin.install "zork1"
     share.install "share/zork1"
